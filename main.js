b20c32a03cd67a131825097df0af630223f2d0e0
Katarzyna Da Silva <conic916@gmail.com>
2019-10-18 11:08:41 +0200

    Add new code js

--- main.js
+++ main.js
@@ -3,21 +3,21 @@
 const h1 = document.querySelector('h1');
 
 document.body.addEventListener('mousemove', function (e) {
-    const x = e.clientX;
-    const y = e.clientY;
+    const x = e.clientX + 1;
+    const y = e.clientY + 1;
     const width = window.innerWidth;
     const height = window.innerHeight;
 
     h1.textContent = x + "," + y;
-document.body.style.backgroundColor = `rgb${red}%, ${green}%,${blue}%`
+    document.body.style.backgroundColor = `rgb${red}%, ${green}%,${blue}%`;
 
-const red = x / width * 100; // 100%;
-const blue = y / height * 100; // 100%;
-const green = 100;
+    const red = x / width * 100; // 100%;
+    const blue = y / height * 100; // 100%;
+    const green = 100;
 
     //h1.textContent = `${e.screenX}, ${e.screenY}`;
     //h1.textContent = `${e.pageX}, ${e.pageY}`;
-     // e. event, .target = mwowi na jakim elem. wykonywane jest zdarzenie
+    // e. event, .target = mwowi na jakim elem. wykonywane jest zdarzenie
 
     //document.body.style.backgroundColor = `rgb(${e.clientX/3}, ${e.clientY/2},${e.clientX/e.clientY * 20})`;
     //  'rgb(' + (e.cllientX / 3) + ',', + (e.clientY/2) +  ',' + (e.clientX *20) + ')' );
